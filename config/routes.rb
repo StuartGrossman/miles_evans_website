971627fb8a9155a93e51634b139dfb50cca5a6ec
Stuart_Grossman <grossman.stuart1@gmail.com>
2015-05-06 10:31:21 -0700

    working 97% needs mobile work

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :udigitize
   resources :medtimes
   resources :writingsample
+  resources :taaka
   root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
