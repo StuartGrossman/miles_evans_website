4e4d267f1e055f46a2da8c90106f8204b5c2c2bb
Stuart_Grossman <grossman.stuart1@gmail.com>
2015-10-11 23:02:08 -0700

    tang added

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :medtimes
   resources :writingsample
   resources :taaka
+  resources :tang
   root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
